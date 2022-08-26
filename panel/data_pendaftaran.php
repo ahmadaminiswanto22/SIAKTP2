d0c1c1c47fd794015f681f451bcbe4f3f8e2f787
ahmadaminiswanto22 <ahmadaminiswanto@gmail.com>
2022-08-26 14:59:08 +0700

    proses detai tampil data pendaftaran

--- panel/data_pendaftaran.php
+++ panel/data_pendaftaran.php
@@ -72,9 +72,9 @@ include 'header.php';
                                                 <td><?= $data['alamat']; ?></td>
                                                 <td><?= $data['jenis_kelamin']; ?></td>
                                                 <!-- <td align="center"><a class="btn btn-primary" type="button" name="view" value="View" data-id="<?php echo $data["nis"]; ?>" class="btn btn-info btn-xs view_data"><i class="fa fa-bars"></i></a></td> -->
-                                                <td><input type="button" name="view" value="View" data-id="<?php echo $data["nis"]; ?>" class="btn btn-info btn-xs view_data"></td>
-                                                <td align="center"><a class="btn btn-warning" type="button" href="edit_pendaftaran.php?nis=<?= $data['nis']; ?>"><i class="fa fa-pencil-square-o"></i></a></td>
-                                                <td align="center"><a class="btn btn-danger" type="button" onclick="return confirm('Data akan di Hapus?')" href="hapus_pendaftaran.php?nis=<?= $data['nis']; ?>"><i class="fa fa-trash-o"></i></a></td>
+                                                <td><a data-toggle="modal" data-target="#detailDaftar" data-id="<?= $data['nis']; ?>" type="submit" class="btn btn-success btn-sm Detai_Pendaftaran">view</a></td>
+                                                <td align="center"><a class="btn btn-warning btn-sm" type="button" href="edit_pendaftaran.php?nis=<?= $data['nis']; ?>"><i class="fa fa-pencil-square-o"></i></a></td>
+                                                <td align="center"><a class="btn btn-danger btn-sm" type="button" onclick="return confirm('Data akan di Hapus?')" href="hapus_pendaftaran.php?nis=<?= $data['nis']; ?>"><i class="fa fa-trash-o"></i></a></td>
                                             </tr>
                                         <?php
                                         }
@@ -88,17 +88,108 @@ include 'header.php';
             </div>
         </div>
     </div>
-    <!-- ----- -->
-    <div id="dataModal" class="modal fade">
-        <div class="modal-dialog">
+    <!-- Modal -->
+    <div class="modal fade" id="detailDaftar" tabindex="-1" aria-labelledby="judulKelas" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">Detail User</h4>
+                    <h5 class="modal-title" id="judulKelas">Data Detail Pendaftaran</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="modal-body" id="detail_user">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <div class="modal-body">
+                    <form action="#" method="post">
+                        <div class="form-group row">
+                            <label for="nis" class="col-md-2 col-form-label col-form-label-sm">NIS</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="nis" id="nis" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nama_siswa" class="col-md-2 col-form-label col-form-label-sm">Nama Siswa</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="nama_siswa" id="nama_siswa" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="alamat" class="col-md-2 col-form-label col-form-label-sm">Alamat</label>
+                            <div class="col-md-9">
+                                <textarea type="text" class="form-control form-control-sm" name="alamat" id="alamat" readonly></textarea>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="jenis_kelamin" class="col-md-2 col-form-label col-form-label-sm">Jenis Kelamin</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="jenis_kelamin" id="jenis_kelamin" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tempat_lahir" class="col-md-2 col-form-label col-form-label-sm">Tempat Lahir</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="tempat_lahir" id="tempat_lahir" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tgl_lahir" class="col-md-2 col-form-label col-form-label-sm">Tanggal Lahir</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="tgl_lahir" id="tgl_lahir" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="status" class="col-md-2 col-form-label col-form-label-sm">Status</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="status" id="status" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nama_negara" class="col-md-2 col-form-label col-form-label-sm">Nama Negara</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="nama_negara" id="nama_negara" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="kelas" class="col-md-2 col-form-label col-form-label-sm">Kelas</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="kelas" id="kelas" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tgl_input" class="col-md-2 col-form-label col-form-label-sm">Tanggal Input</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="tgl_input" id="tgl_input" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="user_input" class="col-md-2 col-form-label col-form-label-sm">User Input</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="user_input" id="user_input" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="tgl_update" class="col-md-2 col-form-label col-form-label-sm">Tanggal Update</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="tgl_update" id="tgl_update" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="user_update" class="col-md-2 col-form-label col-form-label-sm">Tanggal Update</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="user_update" id="user_update" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="akses" class="col-md-2 col-form-label col-form-label-sm">Akses</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control form-control-sm" name="akses" id="akses" readonly>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <a type="button" name="cetak" class="btn btn-primary btn-sm text-white" data-dismiss="modal"><i class="fa fa-print" aria-hidden="true"></i> Cetak</a>
+                            <button type="reset" name="kembali" class="btn btn-secondary btn-sm" data-dismiss="modal">Kembali</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -129,22 +220,51 @@ include 'footer.php';
 <script src="../vendors/pdfmake/build/vfs_fonts.js"></script>
 
 <!-- ----------- -->
-
 <script>
-    $(document).ready(function() {
-        $('.view_data').click(function() {
-            var data_id = $(this).data("nis")
+    $(function() {
+        // $('.tambahKelas').on('click', function() {
+        //     $('#judulKelas').html('Tambah Data Kelas');
+        //     $('.modal-footer button[type=submit]').html('Simpan');
+        //     $('#jenjang').val("--Pilih Jenjang--");
+        //     $('#jurusan').val("--Pilih Jurusan--");
+        //     $('#jmlh').val("");
+        //     $('#nis').val("");
+        // });
+
+        $('.Detai_Pendaftaran').on('click', function() {
+            // $('#judulKelas').html('Edit Data Kelas');
+            // $('.modal-footer button[type=submit]').html('Edit');
+            // $('.modal-body form').attr('action', 'edit_kelas.php');
+            const nis = $(this).data('id');
+            // console.log(id);
+
             $.ajax({
-                url: "proses_detail.php",
-                method: "POST",
+                url: 'proses_detail.php',
                 data: {
-                    data_id: data_id
+                    id: nis
                 },
+                method: 'post',
+                dataType: 'json',
                 success: function(data) {
-                    $("#detail_user").html(data)
-                    $("#dataModal").modal('show')
+                    // console.log(data);
+                    $('#nis').val(data.nis);
+                    $('#nama_siswa').val(data.nama_siswa);
+                    $('#alamat').val(data.alamat);
+                    $('#jenis_kelamin').val(data.jenis_kelamin);
+                    $('#tempat_lahir').val(data.tempat_lahir);
+                    $('#tgl_lahir').val(data.tgl_lahir);
+                    $('#status').val(data.status);
+                    $('#nama_negara').val(data.nama_negara);
+                    $('#kelas').val(data.kelas);
+                    $('#tgl_input').val(data.tgl_input);
+                    $('#user_input').val(data.user_input);
+                    $('#tgl_update').val(data.tgl_update);
+                    $('#user_update').val(data.user_update);
+                    $('#akses').val(data.akses);
+
                 }
-            })
-        })
-    })
+            });
+        });
+
+    });
 </script>
--- panel/proses_detail.php
+++ panel/proses_detail.php
@@ -1,30 +1,13 @@
+
 <?php
 include 'koneksi/koneksi.php';
-if (isset($_POST["data_id"])) {
-    $data_id = $_POST["data_id"];
-    $output = "";
-    $query = "SELECT * FROM pendaftaran WHERE nis = '" . $_POST['nis'] . "'";
-    $result = mysqli_query($conn, $query);
-    $output .= '
-<div class="table-responsive">  
-	<table class="table table-bordered">';
-    $row = mysqli_fetch_array($result);
-    $output .= '  
-          <tr>  
-               <td width="30%"><label>Name</label></td>  
-               <td width="70%">' . $row["nis"] . '</td>  
-          </tr>
-          <tr>  
-               <td width="30%"><label>Gender</label></td>  
-               <td width="70%">' . $row["alamat"] . '</td>  
-          </tr>  
-          <tr>  
-               <td width="30%"><label>Designation</label></td>  
-               <td width="70%">' . $row["status"] . '</td>  
-          </tr>
-          ';
-    $output .= "
-	</table>
-</div>";
-    echo $output;
+
+$nis = $_POST['id'];
+$query = mysqli_query($conn, "SELECT pendaftaran.nis,pendaftaran.nama_siswa,pendaftaran.alamat,pendaftaran.jenis_kelamin,pendaftaran.tempat_lahir,pendaftaran.tgl_lahir,pendaftaran.status,kewarganegaraan.id_negara,kewarganegaraan.nama_negara,agama.id_agama,agama.nama_agama,jurusan.id_jurusan, CONCAT(jenjang.nama_jenjang,' ',jurusan.nama_jurusan) as kelas, pendaftaran.tgl_input,pendaftaran.user_input,pendaftaran.tgl_update,pendaftaran.user_update,CONCAT(user.hak_akses,' (',user.nama,')') as akses FROM pendaftaran INNER JOIN kewarganegaraan ON pendaftaran.id_negara = kewarganegaraan.id_negara JOIN user ON pendaftaran.id_user = user.id_user JOIN agama ON pendaftaran.id_agama = agama.id_agama JOIN jurusan ON pendaftaran.id_jurusan = jurusan.id_jurusan JOIN jenjang ON jurusan.id_jenjang = jenjang.id_jenjang WHERE nis=$nis");
+while ($row = mysqli_fetch_assoc($query)) {
+    $data = $row;
 }
+
+echo json_encode($data);
+
+?>
