04c3aff5822fb44999685db6b19940d22a9c97d2
ahmadaminiswanto22 <ahmadaminiswanto@gmail.com>
2023-07-26 13:47:03 +0700

    perbaikan akhir

---
+++ pendaftaran/login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="demo-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-12 mx-auto">
+        <div class="text-center pb-5"> <img src="https://annedece.sirv.com/Images/circle.png"> </div>
+        <div class="p-5 bg-white rounded shadow-lg">
+          <h2 class="mb-2 text-center">Sign In</h2>
+          <p class="text-center lead">Sign In to manage all your devices</p>
+          <form>
+            <label class="font-500">Email</label>
+            <input name="" class="form-control form-control-lg mb-3" type="email">
+             <label class="font-500">Password</label>
+            <input name="" class="form-control form-control-lg" type="password">
+            <p class="m-0 py-4"><a href="" class="text-muted">Forget password?</a></p>
+            <button class="btn btn-success btn-lg w-100 shadow-lg">SIGN IN</button>
+          </form>
+         <div class="text-center pt-4">
+          <p class="m-0">Do not have an account? <a href="" class="text-dark font-weight-bold">Sign Up</a></p>
+        </div>          
+        </div>        
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
